130a80d7b6322926bbad4baf3a35ef161e8f87b6
sngom1995 <ngomsamba75@gmail.com>
2023-04-29 14:31:55 +0000

    first commit

---
+++ src/main/java/sam/guru/bank/samba_bank/SambaBankApplication.java
@@ -0,0 +1,13 @@
+package sam.guru.bank.samba_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SambaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SambaBankApplication.class, args);
+    }
+
+}
---
+++ src/main/java/sam/guru/bank/samba_bank/controller/AccountController.java
@@ -0,0 +1,4 @@
+package sam.guru.bank.samba_bank.controller;
+
+public class AccountController {
+}
