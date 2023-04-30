cc9aeb6e7d9e7176f6f84b703d49392abb549c1d
sngom1995 <ngomsamba75@gmail.com>
2023-04-30 10:33:33 +0000

    config user in memory details manager

---
+++ src/main/java/sam/guru/bank/samba_bank/config/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package sam.guru.bank.samba_bank.config.security;
+
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+       /*http.authorizeHttpRequests()
+               .requestMatchers("/account/**").authenticated()
+               .requestMatchers("/contact","/notices").permitAll()
+               .anyRequest().permitAll()
+        .and()
+        .formLogin()
+        .and()
+        .httpBasic();*/
+        http.authorizeHttpRequests()
+                .requestMatchers("/account/**").authenticated()
+                .requestMatchers("/contact","/notices").permitAll()
+                .requestMatchers("/balance/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+       return http.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails user = User.withUsername("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+
+    }
+
+}
